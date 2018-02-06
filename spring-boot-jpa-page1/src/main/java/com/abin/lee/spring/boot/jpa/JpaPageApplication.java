1eb3338643cb4412d73fb09c274482971a276e51
zondahuman <zondahuman@gmail.com>
2018-02-06 19:16:27 +0800

    add

--- spring-boot-jpa-page1/src/main/java/com/abin/lee/spring/boot/jpa/JpaPageApplication.java
+++ spring-boot-jpa-page1/src/main/java/com/abin/lee/spring/boot/jpa/JpaPageApplication.java
@@ -14,6 +14,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class JpaPageApplication extends SpringBootServletInitializer {
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(JpaPageApplication.class);
@@ -22,4 +23,6 @@ public class JpaPageApplication extends SpringBootServletInitializer {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(JpaPageApplication.class, args);
     }
+
+
 }
