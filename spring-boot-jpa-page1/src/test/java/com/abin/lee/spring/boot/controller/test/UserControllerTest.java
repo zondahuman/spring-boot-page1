857682cee493770ff13c509bba3563e5fe6d7523
zondahuman <zondahuman@gmail.com>
2018-02-07 13:12:45 +0800

    add

--- spring-boot-jpa-page1/src/test/java/com/abin/lee/spring/boot/controller/test/UserControllerTest.java
+++ spring-boot-jpa-page1/src/test/java/com/abin/lee/spring/boot/controller/test/UserControllerTest.java
@@ -1,29 +1,21 @@
 package com.abin.lee.spring.boot.controller.test;
 
 import com.abin.lee.spring.boot.jpa.JpaPageApplication;
-import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by abin on 2018/2/7 11:55.
@@ -49,9 +41,9 @@ public class UserControllerTest {
 
     @Test
     public void testQ1() throws Exception {
-        String userName = "abin"+(int)(Math.random()*10);
+        String userName = "abin" + (int) (Math.random() * 10);
         String passWord = RandomStringUtils.randomAlphabetic(5);
-        String age = (int)(Math.random()*100) + "";
+        String age = (int) (Math.random() * 100) + "";
 
         mockMvc.perform(MockMvcRequestBuilders.get("/user/add1")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
@@ -61,7 +53,6 @@ public class UserControllerTest {
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("SUCCESS")));
     }
-
 
 
 }
